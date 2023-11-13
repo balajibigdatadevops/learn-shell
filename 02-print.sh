3bd60402c3332a7dd44598cb9d4ab6efa3ccbc08
gbalaji <gadi.balaji@epsilon.com>
2023-11-13 20:02:33 +0530

    adding frontend.sh file to expense-shell repo

---
+++ 02-print.sh
@@ -0,0 +1,14 @@
+echo Hello world
+echo "Hello Balaji, welcome to shell scripting"
+#echo with color codes
+#1. red   31
+#2. green 32
+#3. yellow 33
+#4. blue  34
+#5. maganta 35
+#6. cyan  36
+
+#Syntax: echo -e "\e[color code m message \e[0m"
+
+
+
---
+++ 03-comments.sh
@@ -0,0 +1,24 @@
+#comments
+
+#1. single line comment starts with #
+#2. multiline line comment also we can use every line with # or
+#: '
+#set of statements>
+#'
+
+#single line comment example
+a=10
+echo $a
+a=10
+#echo $a
+
+#multi line comment example
+
+: ' echo Hello World
+a=10
+b=20
+c=30
+'
+
+echo Hello Balaji welcome to shell scripting
+
