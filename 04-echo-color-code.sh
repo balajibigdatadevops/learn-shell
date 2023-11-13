c98b76a7963a925ee92c5c4796b45b00e9e5d5b4
gbalaji <gadi.balaji@epsilon.com>
2023-11-13 20:16:10 +0530

    adding frontend.sh file to expense-shell repo

---
+++ 04-echo-color-code.sh
@@ -0,0 +1,8 @@
+
+#displaying string with different colors
+echo -e "\e[31m Hello Balaji \e[0m"
+echo -e "\e[32m Hello Balaji \e[0m"
+echo -e "\e[33m Hello Balaji \e[0m"
+echo -e "\e[34m Hello Balaji \e[0m"
+echo -e "\e[35m Hello Balaji \e[0m"
+echo -e "\e[36m Hello Balaji \e[0m"
