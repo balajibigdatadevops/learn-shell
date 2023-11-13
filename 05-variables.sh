7796ccd843bcf2f5c4faf4c39bc5913e44f2b03f
gbalaji <gadi.balaji@epsilon.com>
2023-11-13 21:14:34 +0530

    adding frontend.sh file to expense-shell repo

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+a=10
+echo "The value of a is: ${a}"
+
+a=10dollors
+echo "The value is:$a"
+echo "The value is:${a}"
+
+a=10
+b=20
+echo "The addition of ${a} and ${b} is: $((a + b))"
+
+Today_date=$(date)
+echo "today date is: ${Today_date}"
