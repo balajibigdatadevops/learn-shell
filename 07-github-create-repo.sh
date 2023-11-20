c9c0e4843881f9c633769b1e4f782dfe2be50cdb
gbalaji <gadi.balaji@epsilon.com>
2023-11-20 20:14:27 +0530

    create repo using git GLI sh script file

--- 07-github-create-repo.sh
+++ 07-github-create-repo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-which git
+type gh &>/dev/null
 
-if [ $? - -ne 0]; then
+if [ $? -ne 0 ]; then
   curl -L https://cli.github.com/packages/rpm/gh-cli.repo >/etc/yum.repos.d/gh.repo
   yum install gh -y
 fi
 
 gh repo list &>/dev/null
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
   gh auth login && gh repo list
 fi
 
@@ -24,7 +24,3 @@ git branch -m main
 git add -A
 git commit -m INIT
 gh repo create $repo_name --public --source=. --push
-
-
-
-
